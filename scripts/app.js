4465fae974d1352d58984ae7f251356008e4794b
davidtsui <davidtsuiart@gmail.com>
2019-06-02 00:20:44 +0100

    changed to id numbers

--- scripts/app.js
+++ scripts/app.js
@@ -4,18 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10
   const height = 20
   const board = []
-  const activeItem = document.querySelector('.activeItem')
+  const score = document.querySelector('.score')
+
   let playerIndex = 3
   let playerIndexTwo = 4
   let playerIndexThree = 5
   let playerIndexFour = 6
-  // let reachBottom = false
+
 
   function makeGrid(){
     for (let i = 0; i < width * height; i++){
       const square = document.createElement('div')
       square.classList.add('grid-item')
-      square.classList.add(`${i}`)
+      square.setAttribute('id',`${i}`)
       square.innerHTML = i
       board.push(square)
       grid.append(square)
@@ -43,7 +44,6 @@ document.addEventListener('DOMContentLoaded', () => {
     board[playerIndexThree].classList.add('activeItem')
     board[playerIndexFour].classList.add('activeItem')
     bottomCheck()
-    console.log(activeItem)
   }
   setInterval(slowDrop, 1000)
 
@@ -123,7 +123,7 @@ document.addEventListener('DOMContentLoaded', () => {
       board[playerIndexTwo].classList.add('idle')
       board[playerIndexThree].classList.add('idle')
       board[playerIndexFour].classList.add('idle')
-      // reachBottom = true
+
       playerIndex = 3
       playerIndexTwo = 4
       playerIndexThree = 5
@@ -132,12 +132,33 @@ document.addEventListener('DOMContentLoaded', () => {
     //call new shape
   }
 
+  board[190].classList.add('bottom-row')
+  board[191].classList.add('bottom-row')
+  board[192].classList.add('bottom-row')
+  board[193].classList.add('bottom-row')
+  board[194].classList.add('bottom-row')
+  board[195].classList.add('bottom-row')
+  board[196].classList.add('bottom-row')
+  board[197].classList.add('bottom-row')
+  board[198].classList.add('bottom-row')
+  board[199].classList.add('bottom-row')
+
+  function giveScore(){
+    const bottomRow = document.querySelectorAll('.bottom-row')
+    if(bottomRow.forEach(bottomRow => bottomRow.classList.contains('idle'))){
+      score.innerHTML = score + 10
+      console.log('10 points!')
+    }
+  }
+  giveScore()
 
 
+
+  //MUSIC SECTION
+  //MUSIC SECTION
   const audio = document.querySelector('#audio')
   const music = document.querySelector('.music')
   const stopMusic = document.querySelector('.stopMusic')
-  // const currentSong = document.querySelector('#currentSong')
   const tracks = ['sounds/slowyShowdown.mp3','sounds/atompilz.mp3','sounds/Brechmittel.mp3','sounds/Glaubensfrage.mp3']
   music.addEventListener('click', () => {
     audio.src = tracks[Math.floor(Math.random() * tracks.length)]
@@ -148,11 +169,11 @@ document.addEventListener('DOMContentLoaded', () => {
     audio.pause()
     console.log('pressed')
   })
-  // currentSong.innerHTML = `Currently playing: ${tracks}`
+  //MUSIC SECTION
+  //MUSIC SECTION
 
 
   window.addEventListener('keydown', handleKeyDown)
   console.log(board)
-  console.log(activeItem)
 
 })
